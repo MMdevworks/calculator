0bc580a36b43eed428f02a81e124a4bf03f975ae
Marie Marcos <mmdevq@gmail.com>
2024-06-12 21:09:41 -0700

    case d

--- Program.cs
+++ Program.cs
@@ -29,8 +29,15 @@ switch (Console.ReadLine())
         Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
         break;
     case "d":
+        // Ask user to enter a non-zero divisor
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero divisor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
         Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
         break;
+
 }
 
 // Wait for the user to respond before closing.

8e0971820f614bd3f2c6791252757d376f02f83d
Marie Marcos <mmdevq@gmail.com>
2024-06-16 00:01:25 -0700

    clean

--- Program.cs
+++ Program.cs
@@ -1,49 +1,4 @@
 ﻿using System.Text.RegularExpressions;
-// double num1 = 0; double num2 = 0;
-
-// Console.WriteLine("Console Calculator in C#\r");
-// Console.WriteLine("------------------------\n");
-
-// Console.WriteLine("Type a number, and then press Enter");
-// num1 = Convert.ToDouble(Console.ReadLine());
-
-// Console.WriteLine("Type another number, and then press Enter");
-// num2 = Convert.ToDouble(Console.ReadLine());
-
-// Console.WriteLine("Choose an option from the following list:");
-// Console.WriteLine("\ta - Add");
-// Console.WriteLine("\ts - Subtract");
-// Console.WriteLine("\tm - Multiply");
-// Console.WriteLine("\td - Divide");
-// Console.Write("Your option? ");
-
-// // Use a switch statement to do the math.
-// switch (Console.ReadLine())
-// {
-//     case "a":
-//         Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-//         break;
-//     case "s":
-//         Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-//         break;
-//     case "m":
-//         Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-//         break;
-//     case "d":
-//         // Ask user to enter a non-zero divisor
-//         while (num2 == 0)
-//         {
-//             Console.WriteLine("Enter a non-zero divisor: ");
-//             num2 = Convert.ToInt32(Console.ReadLine());
-//         }
-//         Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-//         break;
-
-// }
-
-// // Wait for the user to respond before closing.
-// Console.Write("Press any key to close the Calculator console app...");
-// Console.ReadKey();
 
 class Calculator
 {
